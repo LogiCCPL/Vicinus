2212538a63f9f5e7ad3d88c03b59b2c4b081c952
Robert Adamczyk <robert.adamczyk@ffwagency.com>
2023-05-30 21:37:03 +0200

    First commit

---
+++ Vicinus/VicinusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VicinusApp.swift
+//  Vicinus
+//
+//  Created by Robert Adamczyk on 30.05.23.
+//
+
+import SwiftUI
+
+@main
+struct VicinusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
